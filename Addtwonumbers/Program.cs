96b77c63c0aa398ece8b95813310d72b84e2a7f8
Philip <blah907@gapp.nthu.edu.tw>
2019-07-02 09:46:33 +0800

    Added two more problems

---
+++ Addtwonumbers/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Addtwonumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            ListNode number1 = new ListNode(2);
+            ListNode number2 = new ListNode(5);
+            number1.next= new ListNode(4);
+            number2.next = new ListNode(6);
+            number1.next.next = new ListNode(3);
+            number2.next.next = new ListNode(4);
+            ListNode number3 = addTwoNumbers(number1, number2);
+            Console.WriteLine("Job Complete");
+        }
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int x) { val = x; }
+        }
+
+
+        static ListNode addTwoNumbers(ListNode l1, ListNode l2)
+        {
+            ListNode dummyHead = new ListNode(0);
+            ListNode p = l1, q = l2, curr = dummyHead;
+            int carry = 0;
+            while (p != null || q != null)
+            {
+                int x = (p != null) ? p.val : 0;
+                int y = (q != null) ? q.val : 0;
+                int sum = carry + x + y;
+                carry = sum / 10;
+                curr.next = new ListNode(sum % 10);
+                curr = curr.next;
+                if (p != null) p = p.next;
+                if (q != null) q = q.next;
+            }
+            if (carry > 0)
+            {
+                curr.next = new ListNode(carry);
+            }
+            return dummyHead.next;
+        }
+    }
+}
---
+++ longeststring/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace longeststring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string s = "heuwoqrlaslkslklllsdfjdskcmmdmmmdssdfff";
+            stringData sd = LengthOfLongestSubstring(s);
+            Console.WriteLine(sd.stringLength);
+            Console.WriteLine(s.Substring(sd.start, sd.stringLength));
+        }
+        static stringData LengthOfLongestSubstring(string s)
+        {
+            int n = s.Length;
+            int ans = 0;
+            int start = 0;
+            int end = 0;
+            stringData sd = new stringData();
+            for (int i = 0; i < n; i++)
+                for (int j = i + 1; j <= n; j++)
+                    if (allUnique(s, i, j))
+                    {
+                        if ((j - i) > ans)
+                        {
+                            start = i;
+                            end = j;
+                            ans = Math.Max(ans, j - i);
+                        }
+                    }
+            sd.start = start;
+            sd.end = end;
+            sd.stringLength = ans;
+            return sd;
+        }
+
+        struct stringData
+        {
+            public int start;
+            public int end;
+            public int stringLength;
+        }
+        static bool allUnique(String s, int start, int end)
+        {
+            HashSet<char> set = new HashSet<char>();
+            for (int i = start; i < end; i++)
+            {
+                char ch = s[i];
+                if (set.Contains(ch)) return false;
+                set.Add(ch);
+            }
+            return true;
+        }
+    }
+}
